f2f8e5c9c5ea4c632b74e60befa3b5d0067e2c13
Lokesh Kamanboina <79871646+kamanbo23@users.noreply.github.com>
2025-02-14 13:36:51 -0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -4,10 +4,8 @@
     $password = "A8jwlansi4@";
     $dbname = "ihweb_31074958_imdk";
 
-    // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
